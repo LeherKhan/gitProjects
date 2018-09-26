02f7374f4ba5f8fd464c0070f2314d3abec0fff0
LEHER <hajirairram@gmail.com>
2018-09-27 01:25:26 +0530

    First File

---
+++ Prg1.py
@@ -0,0 +1,10 @@
+ Volume in drive C is Acer
+ Volume Serial Number is BE33-F239
+
+ Directory of C:\Users\Leher\Desktop\Dummy
+
+09/27/2018  01:24 AM    <DIR>          .
+09/27/2018  01:24 AM    <DIR>          ..
+09/27/2018  01:24 AM                 0 Prg1.py
+               1 File(s)              0 bytes
+               2 Dir(s)  88,188,248,064 bytes free
